0b6902779d9416096344664e53552a5912a4cb2b
deepak-gehlot <1deepakgehlot@gmail.com>
2018-01-16 12:47:41 +0530

    added Registration activity and gradle dependency

---
+++ app/src/main/java/com/stufeed/android/view/activity/RegistrationActivity.java
@@ -0,0 +1,15 @@
+package com.stufeed.android.view.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.stufeed.android.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
